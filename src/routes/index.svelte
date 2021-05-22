01dbc7ebaa5da25416bf8a29638d0c19191c58b9
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2021-05-22 18:33:40 +0200

    test2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,6 +37,6 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>deployscript works</h1>
+<h1>deployscript works 2</h1>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
